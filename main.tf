4270668db7259238bb48f31bc8f39aef8950eba0
Romeet Zalani <zalaniromeet@gmail.com>
2025-02-05 21:29:05 +0530

    fix format and add vpc

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   backend "s3" {
-    bucket         = "oozou-test-task-terraform-state"
-    key            = "terraform/state.tfstate"
-    region         = "ap-south-1"  # Change if needed
+    bucket = "oozou-test-task-terraform-state"
+    key    = "terraform/state.tfstate"
+    region = "ap-south-1" # Change if needed
   }
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "oozou-test-task-terraform-state"    # Change to a unique bucket name
+  bucket = "oozou-test-task-terraform-state" # Change to a unique bucket name
   acl    = "private"
 
   tags = {
